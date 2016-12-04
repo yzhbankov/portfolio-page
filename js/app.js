7d90a70c5649596a07df291c12184d2721471dce
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-12-04 22:36:51 +0200

    add mobile friendly layout

--- js/app.js
+++ js/app.js
@@ -20,31 +20,78 @@
 
         $(window).scroll(function () {
             var height = $(window).scrollTop();
-            if (height < 400) {
-                $(".head").css("opacity", (400 - height) / 400);
+            console.log(height);
+            if ($(window).width() > 991) {
+                if (height < 400) {
+                    $(".head").css("opacity", (400 - height) / 400);
+                } else {
+                    $(".head").css("opacity", 0);
+                }
+                if (height > 850) {
+                    $(".navbar").addClass("scrolled");
+                } else {
+                    $(".navbar").removeClass("scrolled");
+                }
+                if (height < 800) {
+                    $('.active').removeClass('active');
+                    $(".home-btn").addClass("active");
+                }
+                if ((height > 800) && (height < 980)) {
+                    $('.active').removeClass('active');
+                    $(".about-btn").addClass("active");
+                }
+                if ((height > 1500) && (height < 1700)) {
+                    $('.active').removeClass('active');
+                    $(".portfolio-btn").addClass("active");
+                }
+                if (height > 1800) {
+                    $('.active').removeClass('active');
+                    $(".contacts-btn").addClass("active");
+                }
+            } else if ($(window).width() < 350) {
+                if (height < 400) {
+                    $(".head").css("opacity", (400 - height) / 400);
+                } else {
+                    $(".head").css("opacity", 0);
+                }
+                if (height < 490) {
+                    $('.active').removeClass('active');
+                    $(".home-btn").addClass("active");
+                }
+                if ((height > 490) && (height < 1450)) {
+                    $('.active').removeClass('active');
+                    $(".about-btn").addClass("active");
+                }
+                if ((height > 1450) && (height < 3700)) {
+                    $('.active').removeClass('active');
+                    $(".portfolio-btn").addClass("active");
+                }
+                if (height > 3700) {
+                    $('.active').removeClass('active');
+                    $(".contacts-btn").addClass("active");
+                }
             } else {
-                $(".head").css("opacity", 0);
-            }
-            if (height > 850) {
-                $(".navbar").addClass("scrolled");
-            } else {
-                $(".navbar").removeClass("scrolled");
-            }
-            if (height < 800) {
-                $('.active').removeClass('active');
-                $(".home-btn").addClass("active");
-            }
-            if ((height > 800) && (height < 980)) {
-                $('.active').removeClass('active');
-                $(".about-btn").addClass("active");
-            }
-            if ((height > 1500) && (height < 1700)) {
-                $('.active').removeClass('active');
-                $(".portfolio-btn").addClass("active");
-            }
-            if (height > 1800) {
-                $('.active').removeClass('active');
-                $(".contacts-btn").addClass("active");
+                if (height < 400) {
+                    $(".head").css("opacity", (400 - height) / 400);
+                } else {
+                    $(".head").css("opacity", 0);
+                }
+                if (height < 580) {
+                    $('.active').removeClass('active');
+                    $(".home-btn").addClass("active");
+                }
+                if ((height > 580) && (height < 1450)) {
+                    $('.active').removeClass('active');
+                    $(".about-btn").addClass("active");
+                }
+                if ((height > 1450) && (height < 3800)) {
+                    $('.active').removeClass('active');
+                    $(".portfolio-btn").addClass("active");
+                }
+                if (height > 3800) {
+                    $('.active').removeClass('active');
+                    $(".contacts-btn").addClass("active");
+                }
             }
         });
         /*-------------------------------------------------------------------------------
@@ -62,7 +109,7 @@
         });
         $(".about-btn").click(function () {
             $('html, body').animate({
-                scrollTop: $(".about").offset().top
+                scrollTop: $(".about").offset().top - 80
             }, 1000);
         });
         $(".portfolio-btn").click(function () {
